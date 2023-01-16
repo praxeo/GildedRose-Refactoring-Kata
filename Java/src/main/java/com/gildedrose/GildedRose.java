d064b4c8166a9a6774d2fcc42c93d885f06b6a2e
Benjamin J. Thompson <bjt@rabidquill.com>
2023-01-15 21:08:10 -0500

    rearranged the tower of if statements into case statements based on the exception products

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -12,52 +12,46 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (item.name.equals(AGED_BRIE) || item.name.equals(BACKSTAGE_PASSES)) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.name.equals(BACKSTAGE_PASSES)) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
+            switch(item.name) {
+                case AGED_BRIE: {
+                    if(item.quality < 50) {
+                        item.quality += 1;
                     }
-                }
-            }
-            else {
-                if (item.quality > 0) {
-                    if (!item.name.equals(SULFURAS)) {
-                        item.quality = item.quality - 1;
+                    item.sellIn -= 1;
+                    if(item.quality < 50 && item.sellIn < 0) {
+                        item.quality += 1;
                     }
+                    break;
                 }
-            }
-
-            if (!item.name.equals(SULFURAS)) {
-                item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.sellIn < 0) {
-                if (!item.name.equals(AGED_BRIE)) {
-                    if (!item.name.equals(BACKSTAGE_PASSES)) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals(SULFURAS)) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
+                case BACKSTAGE_PASSES: {
+                    if(item.quality < 50) {
+                        item.quality += 1;
+                    }
+                    if (item.sellIn < 11 && item.quality < 50) {
+                        item.quality += 1;
+                    }
+                    if (item.sellIn < 6 && item.quality < 50) {
+                        item.quality += 1;
+                    }
+                    item.sellIn -= 1;
+                    if (item.sellIn < 0) {
                         item.quality = 0;
                     }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
+                    break;
+                }
+                case SULFURAS: {
+                    // DO NOTHING
+                    break;
+                }
+                default: {
+                    if (item.quality > 0) {
+                        item.quality -= 1;
                     }
+                    item.sellIn -= 1;
+                    if (item.quality > 0 && item.sellIn < 0) {
+                        item.quality -= 1;
+                    }
+                    break;
                 }
             }
         }
